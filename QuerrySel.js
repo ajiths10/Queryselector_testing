027123a2ae113224a0c91fe5c27cbbf72c0794bf
Nuclear One <ajithsnair10@gmail.com>
2022-02-17 11:04:17 +0530

    Testing query

---
+++ QuerrySel.js
@@ -0,0 +1,21 @@
+// let l=document.querySelector('h1');
+// l.style.color='gray'
+// l.textContent='You are Hacked'
+
+// let b=document.querySelector('input');
+// b.style.color='gray'
+// b.value='hacked';
+
+// var submit=documet.querySelector('input[type="submit"]');
+// submit.value='Hackedd'
+// submit.style.color='gray'
+
+// let bg=document.querySelector('li:nth-child(2)')
+// bg.style.color='green';
+
+let co=document.querySelectorAll('li:nth-child(odd)');
+for(var i=0;i<co.length;i++){
+co[i].style.background='green';
+}
+let bb=document.querySelectorAll('.item');
+bb[1].style.color='green';
